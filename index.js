58071d75e96553502e63c16130aea85cfff8d85b
LHolmberg <lukazlovicc@hotmail.com>
2020-10-16 20:22:36 +0200

    Dsa

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/get/:id", (req, res) => {
   request.end();
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
