c35444a3516e72e55ac961855dc3a92d71dc14a1
LHolmberg <lukazlovicc@hotmail.com>
2020-11-01 01:12:19 +0100

    s

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const app = express();
 var xpath = require('xpath')
   , dom = require('xmldom').DOMParser
 
-app.get("/favicon.ico", (req, res) => {
-  res.send("/favicon.ico");
-});
+  app.get("/favicon.ico", (req, res) => {
+    res.send("/favicon.ico");
+  });
 app.get("/get/:id", (req, res) => {
   var options = {
     host: 'etherscan.io',
@@ -24,9 +24,12 @@ app.get("/get/:id", (req, res) => {
         var node2 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/text()', doc);
         var node3 = xpath.select('//*[@id="content"]/div[1]/div/div[1]/h1/div/span/text()', doc);
 
-        node1.toString() == "" ? res.send("erdsror") : res.send(node1.toString().replace(/,/g, "").replace(/ /g, "") + "," + 
+        node1.toString() == "" ? res.send("error") : res.send(node1.toString().replace(/,/g, "").replace(/ /g, "") + "," + 
         node2.toString().replace(/,/g, "").replace(/ /g, "") + "," + node3.toString().replace(/,/g, "").replace(/ /g, ""));
       });
+  });
+  request.on('error', function (e) {
+      res.send(e.message);
   });
   request.end();
 });
