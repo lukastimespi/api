bcef147f7a932b7b07e1bb8859bd39364f3bd754
lukastimespi <72987813+lukastimespi@users.noreply.github.com>
2020-11-01 14:43:12 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,40 @@
-console.log('Hello world from client-side JS!')
+const http = require('http');
+const express = require("express");
+const fetch = require("whatwg-fetch");
+const app = express();
+var xpath = require('xpath')
+  , dom = require('xmldom').DOMParser
+
+  app.get("/favicon.ico", (req, res) => {
+    res.send("/favicon.ico");
+  });
+app.get("/get/:id", (req, res) => {
+  var options = {
+    host: 'etherscan.io',
+    path: '/token/' + req.params.id
+  }
+  var request = http.request(options, function (rest) {
+      var data = '';
+      rest.on('data', function (chunk) {
+          data += chunk;
+      });
+      rest.on('end', function () {
+        var doc = new dom().parseFromString(data)
+        var node1 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/span[1]/text()', doc);
+        var node2 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/text()', doc);
+        var node3 = xpath.select('//*[@id="content"]/div[1]/div/div[1]/h1/div/span/text()', doc);
+
+        node1.toString() == "" ? res.send("error") : res.send(node1.toString().replace(/,/g, "").replace(/ /g, "") + "," + 
+        node2.toString().replace(/,/g, "").replace(/ /g, "") + "," + node3.toString().replace(/,/g, "").replace(/ /g, ""));
+      });
+  });
+  request.on('error', function (e) {
+      res.send(e.message);
+  });
+  request.end();
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
