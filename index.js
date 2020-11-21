af71683d596c0bed85c650e716db0d49ba7ab9dc
LHolmberg <lukazlovicc@hotmail.com>
2020-11-21 23:03:37 +0100

    t

--- index.js
+++ index.js
@@ -12,9 +12,35 @@ var xpath = require('xpath')
   });
 
   app.get("/test", (req, res) => {
-    cloudscraper.get('https://etherscan.io/token/0xdac17f958d2ee523a2206206994597c13d831ec7').then((value) => {
-      res.send(value);
+    var options = {
+      url: 'https://etherscan.io/token/0xdac17f958d2ee523a2206206994597c13d831ec7',
+      headers: {
+        // User agent, Cache Control and Accept headers are required
+        // User agent is populated by a random UA.
+        'User-Agent': 'Ubuntu Chromium/34.0.1847.116 Chrome/34.0.1847.116 Safari/537.36',
+        'Cache-Control': 'private',
+        'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5'
+      },
+      // Cloudscraper automatically parses out timeout required by Cloudflare.
+      // Override cloudflareTimeout to adjust it.
+      cloudflareTimeout: 5000,
+      // Reduce Cloudflare's timeout to cloudflareMaxTimeout if it is excessive
+      cloudflareMaxTimeout: 30000,
+      // followAllRedirects - follow non-GET HTTP 3xx responses as redirects
+      followAllRedirects: true,
+      // Support only this max challenges in row. If CF returns more, throw an error
+      challengesToSolve: 3,
+      // Remove Cloudflare's email protection, replace encoded email with decoded versions
+      decodeEmails: false,
+      // Support gzip encoded responses (Should be enabled unless using custom headers)
+      gzip: true,
+      // Removes a few problematic TLSv1.0 ciphers to avoid CAPTCHA
+    };
+    
+    cloudscraper(options).then((val) => {
+      res.send(val);
     });
+    
   });
 app.get("/get/:id", (req, res) => {
   
