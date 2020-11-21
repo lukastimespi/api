bd7a47cdff14b6aa07046b0a3c082afa85d4d8e6
LHolmberg <lukazlovicc@hotmail.com>
2020-11-21 22:36:01 +0100

    test

--- index.js
+++ index.js
@@ -4,36 +4,28 @@ const fetch = require("whatwg-fetch");
 const app = express();
 var xpath = require('xpath')
   , dom = require('xmldom').DOMParser
+  const CloudflareBypasser = require('cloudflare-bypasser');
 
+  let cf = new CloudflareBypasser();
   app.get("/favicon.ico", (req, res) => {
     res.send("/favicon.ico");
   });
 app.get("/get/:id", (req, res) => {
-  var options = {
-    host: 'etherscan.io',
-    path: '/token/' + req.params.id,
-    port: null,
-    method: "GET",
-    headers: {'Access-Control-Allow-Origin': 'http://etherscan.io' + '/token/' + req.params.id}
-  }
-  var request = http.request(options, function (rest) {
-      var data = '';
-      rest.on('data', function (chunk) {
-          data += chunk;
-      });
-      rest.on('end', function () {
-        var doc = new dom().parseFromString(data);
-        var node1 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/span[1]/text()', doc);
-        var node2 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/text()', doc);
-        var node3 = xpath.select('//*[@id="content"]/div[1]/div/div[1]/h1/div/span/text()', doc);
-      node1.toString() == "" ? res.send(data) : res.send(node1.toString().replace(/,/g, "").replace(/ /g, "") + "," +
-         node2.toString().replace(/,/g, "").replace(/ /g, "") + "," + node3.toString().replace(/,/g, "").replace(/ /g, ""));
-      });
+  
+  cf.request({
+    url: 'https://etherscan.io/token/0xdac17f958d2ee523a2206206994597c13d831ec7',
+    headers: {
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    }
+  })
+  .then(ress => {
+    var doc = new dom().parseFromString(ress.body);
+    var node1 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/span[1]/text()', doc);
+    var node2 = xpath.select('//*[@id="ContentPlaceHolder1_tr_valuepertoken"]/div/div[1]/span/text()', doc);
+    var node3 = xpath.select('//*[@id="content"]/div[1]/div/div[1]/h1/div/span/text()', doc);
+  node1.toString() == "" ? res.send(data) : res.send(node1.toString().replace(/,/g, "").replace(/ /g, "") + "," +
+     node2.toString().replace(/,/g, "").replace(/ /g, "") + "," + node3.toString().replace(/,/g, "").replace(/ /g, ""));
   });
-  request.on('error', function (e) {
-      res.send(e.message);
-  });
-  request.end();
 });
 
 const PORT = process.env.PORT || 3000;
