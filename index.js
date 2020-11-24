e0a3c34046768e6b3ac3b43b12b47ef73f1e33ab
LHolmberg <lukazlovicc@hotmail.com>
2020-11-24 02:18:23 +0100

    p

--- index.js
+++ index.js
@@ -4,29 +4,35 @@ const fetch = require("whatwg-fetch");
 const app = express();
 var xpath = require('xpath')
   , dom = require('xmldom').DOMParser
+var s = require("@uniswap/sdk");
+var axios = require("axios");
+const { parse } = require('path');
 
 app.get("/favicon.ico", (req, res) => {
   res.send("/favicon.ico");
 });
 
 app.get("/get/:id", (req, res) => {
-  http.get('https://api.coingecko.com/api/v3/coins/ethereum/contract/' + req.params.id, (resp) => {
-  let data = '';
-
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-  resp.on('end', () => {
-    if(JSON.parse(data)["error"] != null) {
+  var sfa = "";
+  axios.get('https://api.etherscan.io/api?module=account&action=tokentx&contractaddress=' + req.params.id + '&page=1&offset=5&apikey=QR62GI9FPA5JQ38576IVBWP53P9MBYYK7V').then(function (responses) { 
+    const DAI = new s.Token(s.ChainId.MAINNET, req.params.id, responses.data.result[0].tokenDecimal)
+    s.Fetcher.fetchPairData(DAI, s.WETH[DAI.chainId]).then((ressu) => {
+      axios.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
+    .then(function (response) {
+      const route = new s.Route([ressu], s.WETH[DAI.chainId])
+      res.send("@"+route.midPrice.invert().toSignificant(6) + "Eth" + ",$" + String(parseFloat(String(response.data.USD)* parseFloat(String(route.midPrice.invert().toSignificant(6))))) + "," + responses.data.result[0].tokenName);
+    })
+    .catch(function (error) {
+      res.send("error")
+    })
+    .then(function () {
+    });
+    }).catch((error) => {
       res.send("error");
-    } else {
-      res.send("@" + JSON.parse(data)["market_data"]["current_price"]["eth"] + "Eth" + "," + "$" + JSON.parse(data)["market_data"]["current_price"]["usd"] + "," + JSON.parse(data)["name"]);
-    }
-  });
-
-}).on("error", (err) => {
-  res.send("error");
-});
+    });
+    }).catch(function (error) {
+      res.send("error");
+    });
 });
 //@0.001678Eth, $0.9979 ,TetherUSD
 const PORT = process.env.PORT || 3000;
